e1e536fe2edcc947530f8fbf2cda25287c94ce43
Vibhav <vibhavsachan743@gmail.com>
2023-07-30 23:57:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import SearchResult from "./components/SearchResult";
+import { AppContext } from "./utils/ContextApi";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/" exact element={<SearchResult />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
